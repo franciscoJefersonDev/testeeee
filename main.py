8f77eaf1de8e096d9fb1dbf10a749fce0438238b
franciscoJefersonDev <francisco.jeferson.dev@gmail.com>
2022-09-25 11:28:25 -0300

    11:28-2022-09-25

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+from datetime import date, datetime
+
+gitAccessToken = ''
+time = datetime.now().strftime("%H:%M")
+date = date.today()
+commit = '{time}-{date}'.format(time=time, date=date)
+
+# print(commit)
+# with open('/home/francisco/Área de Trabalho/git-access-token.txt') as f:
+#     gitAccessToken = f.readlines()[0]
+#     print(gitAccessToken)
+
+os.system('git add .')
+os.system('git commit -m {commit}'.format(commit=commit))
